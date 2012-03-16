b0f0ea10dfc8fc4c7d93dbf0a8d777e765c3cbd3
vagmi <vagmi.mudumbai@gmail.com>
2012-03-16 16:36:41 +0530

    add basic auth for buckets

--- app/controllers/buckets_controller.rb
+++ app/controllers/buckets_controller.rb
@@ -1,12 +1,14 @@
 class BucketsController < ApplicationController
+  before_filter :authenticate_user!
   def index
-    @buckets = Bucket.all
+    @buckets = current_user.buckets
   end
   def new
-    @bucket = Bucket.new
+    @bucket = current_user.buckets.build
   end
   def create
     @bucket = Bucket.new(params[:bucket])
+    @bucket.user = current_user
     if(@bucket.save)
       redirect_to buckets_path, :notice=>"Bucket created successfully"
     else
--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -2,4 +2,5 @@ class Bucket < ActiveRecord::Base
   has_many :items
   validates_presence_of :name
   belongs_to :user
+  attr_accessible :name
 end
