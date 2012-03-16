7492fc28959b4878207564b51a720fae5d291662
vagmi <vagmi.mudumbai@gmail.com>
2012-03-16 12:42:31 +0530

    crud for items complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,36 @@ class ItemsController < ApplicationController
     @bucket = Bucket.find(params[:bucket_id])
     @items = @bucket.items
   end
+  def new
+    @bucket = Bucket.find(params[:bucket_id])
+    @item = @bucket.items.build
+  end
+  def create
+    @bucket = Bucket.find(params[:bucket_id])
+    @item = @bucket.items.build(params[:item])
+    if(@item.save)
+      redirect_to bucket_items_path(@bucket), :notice=>"Item added successfully"
+    else
+      render :action=>:new
+    end
+  end
+  def edit
+    @item = Item.find(params[:id])
+    @bucket = @item.bucket
+  end
+  def update
+    @item = Item.find(params[:id])
+    @bucket = @item.bucket
+    if(@item.update_attributes(params[:item]))
+      redirect_to bucket_items_path(@bucket), :notice=>"Item updated successfully"
+    else
+      render :action=>:edit
+    end
+  end
+  def destroy
+    @item = Item.find(params[:id])
+    @bucket = @item.bucket
+    @item.destroy
+    redirect_to bucket_items_path(@bucket), :notice=>"Item deleted successfully"
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ActiveRecord::Base
   belongs_to :bucket
   has_many :taggings
   has_many :tags, :through=>:taggings
+  validates_presence_of :content
 end
